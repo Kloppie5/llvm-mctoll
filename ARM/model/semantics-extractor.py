b146e63e7763755e02cdbe3207231e3b2df23a93
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2021-10-25 19:18:01 +0200

    [ARM?] py and ASL cleanup

--- ARM/model/semantics-extractor.py
+++ ARM/model/semantics-extractor.py
@@ -1,22 +1,29 @@
 from lxml import etree as ET
+import re
 import os
 
-semantics_files = []
-for root, dirs, files in os.walk("./operational_semantics/"):
-  for file in files:
-    if file.endswith(".xml"):
-      semantics_files.append(os.path.join(root, file))
-print("Found %d semantics files" % len(semantics_files))
+# get files
+def get_files(path):
+  semantics_files = []
+  for root, dirs, files in os.walk(path):
+    for file in files:
+      if file.endswith(".xml"):
+        semantics_files.append(os.path.join(root, file))
+  print("Found %d semantics files" % len(semantics_files))
+  return semantics_files
 
-ISAs = {}
-ISAs["A32"] = []
-ISAs["T32"] = []
-for semantics_file in semantics_files:
-  print("Processing %s" % semantics_file)
+def parse_semantics(semantics_file):
   xml = ET.parse(semantics_file)
+  instructions = []
+  if xml.find('.//aliasto') is not None:
+    print("Skipping %s" % semantics_file)
+    return instructions
+  print("Processing %s" % semantics_file)
   for iclass in xml.findall('.//classes/iclass'):
+    instruction = {}
     isa = iclass.attrib['isa']
-    mnemonics = set()
+    instruction['isa'] = isa
+
     match_pattern = []
     print_pattern = []
     constraints = []
@@ -42,17 +49,48 @@ for semantics_file in semantics_files:
       if constraint is not None:
         constraints.append("%s %s" % (name, constraint))
       print_pattern.append(pattern)
+    instruction['match_pattern'] = match_pattern
+    instruction['print_pattern'] = print_pattern
+    instruction['constraints'] = constraints
 
+    mnemonics = set()
     for encoding in iclass.findall('encoding'):
       mnemonic = encoding.xpath('./docvars/docvar[@key="mnemonic"]/@value')[0]
       if mnemonic is not None:
         mnemonics.add(mnemonic)
-    ISAs[isa].append({
-      "mnemonics" : mnemonics,
-      "match_pattern" : match_pattern,
-      "print_pattern" : print_pattern,
-      "constraints" : constraints
-    })
+    instruction['mnemonics'] = mnemonics
+
+    pseudocode_decode = cleanup_ASL(ET.tostring(iclass.find('ps_section/ps/pstext')).decode())
+    pseudocode_execute = cleanup_ASL(ET.tostring(xml.find('ps_section/ps/pstext')).decode())
+
+    instruction['psuedocode'] = (pseudocode_decode, pseudocode_execute)
+
+    instructions.append(instruction)
+  return instructions
+
+tag_re = re.compile(r'<\/?a[^>]*>')
+def cleanup_ASL(asl):
+  asl = asl.replace('<pstext mayhavelinks="1" section="Decode" rep_section="decode">', '')
+  asl = asl.replace('<pstext mayhavelinks="1" section="Execute" rep_section="execute">', '')
+  asl = asl.replace('</pstext>', '')
+
+  asl = asl.replace('&lt;', '<')
+  asl = asl.replace('&gt;', '>')
+  asl = asl.replace('&amp;', '&')
+
+  asl = tag_re.sub('', asl)
+
+  return asl
+
+semantics_files = get_files("./operational_semantics/")
+
+ISAs = {}
+ISAs["A32"] = []
+ISAs["T32"] = []
+for semantics_file in semantics_files:
+  instructions = parse_semantics(semantics_file)
+  for instruction in instructions:
+    ISAs[instruction["isa"]].append(instruction)
 
 for isa in ISAs:
   print("%s:" % isa)
@@ -61,3 +99,5 @@ for isa in ISAs:
     print("  %s" % ''.join(instruction["match_pattern"]))
     print("  %s" % ' '.join(instruction["print_pattern"]))
     print("  %s" % ', '.join(instruction["constraints"]))
+    print("  %s" % instruction["psuedocode"][0])
+    print("  %s" % instruction["psuedocode"][1])
